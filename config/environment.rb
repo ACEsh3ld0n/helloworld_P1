4863b198743b7a6a5660351e484f2c2aa28e83d5
Sheldon <legendkoala@gmail.com>
2012-02-11 02:48:12 +0200

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+HelloworldP1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloworldP1::Application.config.secret_token = '33387de6cba0fac7581605f5867ee5a0c6914d8b7bcc09e0460ab14653f151bb7f09abbe82157255e9c799f9f1617eb7c947f53c0d061ac09afa2e8d8fe15a00'
