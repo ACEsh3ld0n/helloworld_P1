fa13879a9b91a93ea102ad869a5e0a9d5b41ca49
Sheldon <legendkoala@gmail.com>
2012-02-17 13:02:44 +0200

    Error Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def render_404
+    render :template => 'errors/404.html.erb', :status => 404
+  end
+  
+  def render_500
+    render :template => 'errors/500.html.erb', :status => 500
+  end
 end
