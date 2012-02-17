a543f57ed34249e10dda0add7892898dbb51ea86
Sheldon <legendkoala@gmail.com>
2012-02-17 13:15:25 +0200

    Error Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+    unless ActionController::Base.consider_all_requests_local
+    rescue_from Exception, :with => :render_error
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    rescue_from ActionController::RoutingError, :with => :render_not_found
+    rescue_from ActionController::UnknownController, :with => :render_not_found
+    rescue_from ActionController::UnknownAction, :with => :render_not_found
+  end 
+  
   def render_optional_error_file(status_code)
         status = interpret_status(status_code)
         render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'application.html.erb'
