6d25a96f22116eb219433d071fbc840167f03367
Sheldon <legendkoala@gmail.com>
2012-02-17 13:12:14 +0200

    Error Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  def render_optional_error_file(status_code)
+        status = interpret_status(status_code)
+        render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'application.html.erb'
+  end
+  
   def render_404
     render :template => 'errors/404.html.erb', :status => 404
   end
